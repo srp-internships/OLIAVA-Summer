591206974adeb9d50b1719d0275a0643c9b4d126
Avazbek Olimov <84025676+Avazbek22@users.noreply.github.com>
2023-07-24 10:56:48 +0500

    Blazor project
    Learning the Blazor

---
+++ BlazorEcommerce/BlazorEcommerce/Server/Program.cs
@@ -0,0 +1,54 @@
+global using Microsoft.AspNetCore.ResponseCompression;
+global using BlazorEcommerce.Shared;
+global using Microsoft.EntityFrameworkCore;
+using BlazorEcommerce.Server.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//Console.WriteLine(builder.Configuration.GetConnectionString("DefaultConnection"));
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json","My API V1");
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
